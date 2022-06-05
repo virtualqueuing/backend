993c7b68286ea67536509d48ad47ef87cfeec7b4
wanyansu <wanyansuroy@gmail.com>
2022-06-05 22:34:35 +1000

    feat: database set up and connected

--- controllers/service.js
+++ controllers/service.js
@@ -12,35 +12,42 @@ const addService = async (req, res) => {
 };
 
 const addQueue = async (req, res) => {
-  const { name, phoneNumber, queueNumber, guestsNumber, tableSize, notes } = req.body;
+  const { name, phoneNumber, guestsNumber, tableSize, notes } = req.body;
   const currentDate = new Date(Date.now());
   const matchingDate = `${currentDate.getYear() + 1900}-${
     currentDate.getMonth() + 1
   }-${currentDate.getDate()}`;
   // logic to check if database with today's date exists
-  const todayService = await Service.findOne({ dateString: matchingDate }).exec();
-  await todayService.queues.push({
-    name,
-    phoneNumber,
-    queueNumber,
-    guestsNumber,
-    tableSize,
-    notes,
-  });
-  await todayService.save();
-  return res.status(201).json(todayService);
+  try {
+    const todayService = await Service.findOne({ dateString: matchingDate }).exec();
+    const currentQueueNumber = todayService.queues.length;
+    await todayService.queues.push({
+      name,
+      phoneNumber,
+      queueNumber: currentQueueNumber + 1,
+      guestsNumber,
+      tableSize,
+      notes,
+    });
+    await todayService.save();
+    return res.status(201).json(todayService);
+  } catch (err) {
+    return res.status(500).json(err);
+  }
 };
 
-// const getQueueById = async (req, res) => {
-//   const { id } = req.params;
-//   const queue = await Service.aggregate([
-//     { $unwind: '$queues' },
-//     { $match: { 'queues._id': id } },
-//   ]).exec();
-//   if (!queue) {
-//     return res.status(404).json({ error: 'Queue Not Found!' });
-//   }
-//   return res.json(queue);
-// };
+const getQueueById = async (req, res) => {
+  const { id } = req.params;
+  const currentDate = new Date(Date.now());
+  const matchingDate = `${currentDate.getYear() + 1900}-${
+    currentDate.getMonth() + 1
+  }-${currentDate.getDate()}`;
+  const todayService = await Service.findOne({ dateString: matchingDate }).exec();
+  const queue = todayService.queues.id(id);
+  if (!queue) {
+    return res.status(404).json({ error: 'Queue Not Found!' });
+  }
+  return res.json(queue);
+};
 
-module.exports = { getAllQueues, addService, addQueue };
+module.exports = { getAllQueues, addService, addQueue, getQueueById };
--- routes/queue.js
+++ routes/queue.js
@@ -1,13 +1,13 @@
 const { Router } = require('express');
 
-const { getAllQueues, addQueue, addService } = require('../controllers/service');
+const { getAllQueues, addQueue, addService, getQueueById } = require('../controllers/service');
 
 const queueRouter = Router();
 
 queueRouter.get('', getAllQueues);
 queueRouter.post('', addQueue);
 queueRouter.get('/initiate', addService);
-// queueRouter.get('/:id', getQueueById);
+queueRouter.get('/:id', getQueueById);
 // queueRouter.put('/:code', updateProductByCode);
 
 module.exports = queueRouter;
