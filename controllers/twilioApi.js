994670efbc7830262dbddddca66641d564982ea9
Yangsu Wang <wanyansuroy@gmail.com>
2022-06-10 03:51:45 +0000

    Merged in revert-pr-16 (pull request #17)
    Revert "feat: add twilio route request to Swagger api (pull request #16)"

--- controllers/twilioApi.js
+++ controllers/twilioApi.js
@@ -6,28 +6,11 @@ const twilioPhoneNumber = process.env.TWILIO_PHONE_NUMBER;
 
 const client = require('twilio')(accountSid, authToken);
 
-/**
- * @swagger
- * /v1/handleText:
- *  get:
- *    summary: Get testing text message from Twilio
- *    tags: [Twilio]
- *    responses:
- *      200:
- *        description: receving a text message 'Welcome to Virtual Queuing' from twilio number by clicking Execute button. Please make sure the recipient's phnoe number is verified.
- *        content:
- *          application/json:
- *            schema:
- *              type: object
- *              items:
- *                message: 'Text sent!'
- */
-
 const sendText = (req, res) => {
   client.messages
     .create({
-      body: 'Welcome to Virtual Queuing',
-      to: '+61416561624', // Ting's number
+      body: 'Welcome to Virtual Queuing - Thanks for saving me. Happy message from Ting~~',
+      to: '+61433574889', // Roy's number
       from: twilioPhoneNumber,
     })
     .then((message) => console.log(message.sid))
