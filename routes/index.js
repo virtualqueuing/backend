a730face40eb1a46b9a7a16931de03f7c6c28e9c
Ting Huang <tinghuang.dev@gmail.com>
2022-06-28 04:24:32 +0000

    Merged in VQ-49-write-login-and-register-function-backend (pull request #25)
    VQ-49 write login and register function backend
    
    * feat: uncomment auth router and add requied documents in user schema
    
    * feat: uncomment auth router and add requied documents in user schema
    
    
    Approved-by: Yangsu Wang

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ const express = require('express');
 const queueRouter = require('./queue');
 const sendTextRouter = require('./twilio');
 // const productRouter = require('./product');
-// const authRouter = require('./auth');
+const authRouter = require('./auth');
 
 const v1Router = express.Router();
 
-// v1Router.use('/auth', authRouter);
+v1Router.use('/auth', authRouter);
 // v1Router.use('/products', productRouter);
 v1Router.use('/queues', queueRouter);
 v1Router.use('/handleText', sendTextRouter);
