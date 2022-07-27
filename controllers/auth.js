fc221e5889f386d6980161b570a1b38318949695
Ting Huang <tinghuang.dev@gmail.com>
2022-07-27 12:46:31 +0000

    Merged in VQ-161-fix-adjust-status-code-to-fix-signup-error-message (pull request #42)
    VQ-161 fix adjust status code to fix signup error message
    
    * feat: adding conflict error func to adjust duplicated email error status code
    
    * feat: adding conflict error func to adjust duplicated email error status code
    
    
    Approved-by: Yangsu Wang

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,30 +1,26 @@
 const { StatusCodes } = require('http-status-codes');
 const User = require('../models/User');
-const { BadRequestError, UnauthenticatedError } = require('../errors');
+const { BadRequestError, UnauthenticatedError, ConflictError } = require('../errors');
 const { generateToken } = require('../utils/jwt');
 
 const register = async (req, res) => {
   const user = new User({ ...req.body });
-  const repeatUser = await User.findOne({ data: user });
 
-  if (user.email === repeatUser.email) {
-    res
-      .status(400)
-      .json([{ email: user.email }, { error: 'This email address is already being used' }]);
+  if (!(await User.findOne({ email: user.email }))) {
+    await user.save();
+    const token = generateToken({ user });
+    return res.status(StatusCodes.CREATED).json({
+      data: {
+        email: user.email,
+        role: user.role,
+        fullName: user.fullName,
+        branch: user.branch,
+        _id: user._id,
+      },
+      token,
+    });
   }
-
-  await user.save();
-  const token = generateToken({ user });
-  return res.status(StatusCodes.CREATED).json({
-    data: {
-      email: user.email,
-      role: user.role,
-      fullName: user.fullName,
-      branch: user.branch,
-      _id: user._id,
-    },
-    token,
-  });
+  throw new ConflictError('This email address is already being used');
 };
 
 const login = async (req, res) => {
