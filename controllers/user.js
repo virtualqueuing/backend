c4a73aa6777487d927360454f339232f7e7b1152
Ting Huang <tinghuang.dev@gmail.com>
2022-08-02 01:53:28 +0000

    Merged in VQ-168-feat-add-put-method-on-backend-for-changing-user-details-on-profile-page (pull request #43)
    feat: put method for updating user details
    
    * feat: put method for updating userdatail
    
    
    Approved-by: Yangsu Wang

--- controllers/user.js
+++ controllers/user.js
@@ -7,38 +7,43 @@ const getUserById = async (req, res) => {
   try {
     const user = await User.findById(id);
     const { password, ...others } = user._doc;
+
     return res.status(StatusCodes.OK).json(others);
   } catch (err) {
     return res.status(StatusCodes.NOT_FOUND).json(err);
   }
 };
 
-// TODO: Follow up the below function
-// const updateUserById = async (req, res) => {
-//   const { id } = req.params;
-//   if (req.body.id === id) {
-//     if (req.body.password) {
-//       if (comparePassword(req.body.password)) {
-//         try {
-//           const updatedUser = await User.findByIdAndUpdate(
-//             id,
-//             {
-//               $set: req.body,
-//             },
-//             { new: true }
-//           );
-//           return res.status(StatusCodes.OK).json(updatedUser);
-//         } catch (err) {
-//           return res.status(StatusCodes.INTERNAL_SERVER_ERROR).json(err);
-//         }
-//       } else {
-//         return res.status(StatusCodes.UNAUTHORIZED).json('You can only update your own account!');
-//       }
-//     }
-//   }
-// };
+const updateUserById = async (req, res) => {
+  const authHeader = req.headers.authorization;
+  const returnToken = authHeader.split(' ')[1];
+
+  const { id } = req.params;
+
+  try {
+    const user = await User.findByIdAndUpdate(
+      id,
+      {
+        $set: req.body,
+      },
+      { new: true }
+    );
+    return res.status(StatusCodes.OK).json({
+      data: {
+        email: user.email,
+        role: user.role,
+        fullName: user.fullName,
+        branch: user.branch,
+        _id: user._id,
+      },
+      token: returnToken,
+    });
+  } catch (err) {
+    return res.status(StatusCodes.NOT_FOUND).json(err);
+  }
+};
 
 module.exports = {
   getUserById,
-  // updateUserById,
+  updateUserById,
 };
--- routes/index.js
+++ routes/index.js
@@ -10,6 +10,6 @@ const v1Router = express.Router();
 v1Router.use('/auth', authRouter);
 v1Router.use('/queues', auth, queueRouter);
 v1Router.use('/sendtext', sendTextRouter);
-v1Router.use('/user', userRouter);
+v1Router.use('/user', auth, userRouter);
 
 module.exports = v1Router;
